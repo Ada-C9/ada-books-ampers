415822501df742736f4a5abc91ba73c2171974bc
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-20 14:08:20 -0700

    did migrations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,12 @@
 class BooksController < ApplicationController
 
-  BOOK_LIST = [
-      {id: 1, title: "1984", author: "George Orwell", review: "4/5. Had to read and nearly finished"},
-      {id: 2, title: "Pride & Prejudice", author: "Jane Austin", review: "4.2/5. Never read but people seem to love it."},
-      {id: 3, title: "Harry Potter", author: "JK Rowling", review: "5/5. Greatest book ever."},
-      {id: 4, title: "Harry Poodr", author: "Sandy Metz", review: "TBD. Chapter 3 was excessive."}
-    ]
-
   def index
-    @books = BOOK_LIST
+    @books = Book.all
   end
 
   def show
     id = params[:id]
-    @book = BOOK_LIST.find { |book| book[:id] == id.to_i }
+    @book = Book.all.find { |book| book.id == id.to_i }
   end
 
   def new
@@ -29,5 +22,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    @book = Book.all.find { |book| book.id == id.to_i }
+    @book.destroy
   end
+
+
 end
